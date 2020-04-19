21e43fc78135d284f5b49613a6b51d91ded28e6b
Tarun <tarun@dailyobjects.com>
2020-04-19 10:59:53 +0530

    Part - 1, Setting Up and Displaying Lists ✅

---
+++ Components/TodoList.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import colors from '../Colors'
+
+export default TodoList = ({ list }) => {
+    const completedCount = list.todos.filter(todo => todo.completed).length;
+    const remainingCount = list.todos.length - completedCount;
+
+    return (
+        <View style={[styles.listContainer, { backgroundColor: list.color }]}>
+            <Text style={styles.listTitle} numberOfLines={1} >
+                {list.name}
+            </Text>
+            <View>
+                <View style={{ alignItems: "center" }}>
+                    <Text style={styles.count}>{completedCount}</Text>
+                    <Text style={styles.subTitle}>Remaining</Text>
+                </View>
+
+                <View style={{ alignItems: "center" }}>
+                    <Text style={styles.count}>{remainingCount}</Text>
+                    <Text style={styles.subTitle}>Completed</Text>
+                </View>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    listContainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 16,
+        borderRadius: 6,
+        marginHorizontal: 12,
+        alignItems: "center",
+        width: 200
+    },
+    listTitle: {
+        fontSize: 24,
+        fontWeight: "700",
+        color: colors.white,
+        marginBottom: 18
+    }, count: {
+        fontSize: 48,
+        fontWeight: "200",
+        color: colors.white,
+    },
+    subTitle: {
+        fontSize: 12,
+        fontWeight: "700",
+        color: colors.white
+    }
+})
---
+++ tempData.js
@@ -0,0 +1,66 @@
+// Temporary Data until we connect with firebase
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Walk 2 KM",
+                completed: true
+            },
+            {
+                title: "Learn React Native",
+                completed: true
+            },
+            {
+                title: "Plan Weekend",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Ballons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: true
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            }
+        ]
+    }
+]
